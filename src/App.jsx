0dd3c4c40f5b564926f76558b02addd098f65381
Furkan Demirtaş <furkan-demirtas@outlook.com>
2025-02-21 14:55:31 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,27 @@
  * ⚠️ BAŞLAMADAN ÖNCE YAPILACAKLAR
  * -------------------------------
  * 1. Aşağıdaki komutlardan birini terminal penceresine kopyalayıp yapıştırın:
+ *
+ *    📌 Windows PowerShell kullanıyorsanız:
+ *    > Remove-Item "README.md","public\delete_this_file",".git" -Recurse -Force; git init
  * 
- *    📌 Windows CMD kullanıyorsanız (PowerShell değil):
+ *    📌 Windows CMD kullanıyorsanız:
  *    > del README.md && del public\delete_this_file && rmdir /s /q .git && git init
- *    
- *    📌 Git Bash kullanıyorsanız:
- *    > rm README.md public/delete_this_file && rm -rf .git && git init
  * 
+ *    📌 Bash kullanıyorsanız:
+ *    > rm README.md public/delete_this_file && rm -rf .git && git init
+ *
  * 2. package.json Düzenleyin:
  *    - "name" alanını projenizin adıyla değiştirin
  *    - Proje adı küçük harflerle ve tire (-) ile yazılmalıdır
  *    - Örnek: "my-awesome-project"
  */
 
-export default function App() {
+const App = () => {
   return (
     <>
     </>
   );
 };
+
+export default App;
